2ddbeac536c6578d79a1c8c0c93f8c80a52ed9ad
githubmave <chanzuidan@gmail.com>
2021-09-25 19:58:11 +1200

    af

--- routers.js
+++ routers.js
@@ -2,7 +2,8 @@
 //This file is to grep data from data object and deliver them to the front end
 const express = require('express')
 const router = express.Router()
-const {findPatient} = require('./coneTbl')
+const {findPatient,findPatientById,addPatient,updatePatn,deletePatn
+} = require('./coneTbl')
 
 router.get('/',(req,res)=>{
        
