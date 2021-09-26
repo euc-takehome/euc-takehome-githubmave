32bdef6e63c64c5bef34f9d65464172444b59148
githubmave <chanzuidan@gmail.com>
2021-09-26 13:20:47 +1300

    fa

--- seeds/questionnaire_question2.js
+++ seeds/questionnaire_question2.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('questionnaire_question2').insert([
-        {id: 77701, description: 'rWhat is your blood pressure?',short_code:'code1'},
-        {id: 77702, description: 'What is your blood suger?',short_code:'code2'},
-        {id: 77703, description: 'Do you have headache?',short_code:'code3'}
+        {id: 77701, description: 'What is your blood pressure?',short_code:'code1',patient_id:88801},
+        {id: 77702, description: 'What is your blood suger?',short_code:'code2',patient_id:88802},
+        {id: 77703, description: 'Do you have headache?',short_code:'code3',patient_id:88803}
       ]);
     });
 };
